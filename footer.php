77193aaad149589177e15d37617cf3353e047ce2
sushant-hiray <hiraysushant@gmail.com>
2013-08-03 14:41:36 +0530

    Added header,footer,logo

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Footer for all pages
+ */
+?>  <div id="push"></div>
+    </div> <!-- /wrap -->
+    <div id="footer">
+      <div class="container">
+        <p class="muted credit">Built with love by <a href="about.php">@sushant @ashish @nilesh.</p>
+      </div>
+    </div>
+
+    <!-- javascript files
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body></html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Header for user pages
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>WnCC Prog Test Bed </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/common.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+    <!-- fav and touch icons -->
+    <link rel="shortcut icon" href="http://twitter.github.com/bootstrap/assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+<!-- Part 1: Wrap all page content here -->
+    <div id="wrap">
+
+      <!-- Fixed navbar -->
+      <div class="container-fluid">
+        <header class="row-fluid">
+          
+          <div class="span2">
+            <img src="img/logo.jpg" width="120" class="img-polaroid">
+          </div>
+          
+          <div class="span10">
+              <div class="row-fluid">
+                <div class="span10">
+                  <h3 align="center">Car Pooling</h3>
+                </div>
+                <div class="span2">
+                    <a class="btn btn-info" href="index.php" role="button">CarPool Home</a>
+                </div>
+              </div>
+        </div>  
+      </div> 
+
+      <!-- Begin page content -->
