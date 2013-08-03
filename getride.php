277bb019a7f3a31255a1a6f0d72a7d6abc0339bd
sushant-hiray <hiraysushant@gmail.com>
2013-08-03 22:08:18 +0530

    UI for get ride done, minor layout change in index

--- getride.php
+++ getride.php
@@ -10,20 +10,13 @@
 		connectdb();
 ?>
 <div class="container">
-	<?php
-        if(isset($_GET['share']))
-          echo("<div class=\"alert alert-info\">\nYour Ride was succesfully added! You can edit it from your profile\n</div>");
-        else if(isset($_GET['nerror']))
-          echo("<div class=\"alert alert-error\">\nPlease enter all the details asked before you can continue!\n</div>");
-      ?>
-
 <?php
 include('menu.php');
 ?>
 	<div class="row-fluid" id="main-content">
 		<div class="span1"></div>
 		<div class="span5"> 
-			<h2 align="center"><small>Search for a ride</small></h2>
+			<h2 align="center"><small>Search for a preferred ride</small></h2>
 			<hr>
       		<br/>
 			<form id="search">
@@ -61,29 +54,9 @@ include('menu.php');
       
 		</div>
 		<div class="span5">
-			<h2 align="center"><small>Latest Car Pools</small></h2>
-			<?php
-						$today = date("Y-m-d H:i:s");
-						$query="SELECT `from` , `to` , `uptime` , `vehicle` from offers where uptime > '".$today."'";
-						$result = mysql_query($query);
-						if(mysql_num_rows($result)==0){
-		          	    	echo("<p align='center'>No Upcoming car pools are scheduled currently :( </p>\n");
-		          	  
-								
-							}
-		          	  	else {
-							echo '<table id="upcominglist" class="table table-hover">
-								<thead><tr> <th>Vehicle Type</th> <th> From </th> <th> To </th> <th> Starting Time</th></tr></thead>
-								<tbody>';
-									
-							while($row = mysql_fetch_array($result)) {
-								echo "<tr><td>".$row['vehicle']."</td><td>".$row['from']."</td><td>".$row['to']."</td><td>".$row['uptime']."</td></tr>";
-							}
-						}
-					?>
-					</tbody>
-			</table>
-		</div>
+      <h2 align="center"><small>Search Results</small></h2> <hr/>
+
+    </div>
 	</div>
 </div>
 <div id="push"></div>
